c0a3a6d3319183b8f8b6b60f0655e636f7b578a6
Steven Wohl <steverwohl@gmail.com>
2018-08-07 11:13:36 +0200

    added some new features like static and updated the python to 3.6

--- micromic/micromic/settings.py
+++ micromic/micromic/settings.py
@@ -71,6 +71,9 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'builtins': [
+                'django.contrib.staticfiles.templatetags.staticfiles',
+            ],
         },
     },
 ]
@@ -135,3 +138,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
--- venv/bin/django-admin.py
+++ venv/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/home/swohl/repo/django/micromet/venv/bin/python3.5
+#!/home/swohl/repo/django/micromet/venv/bin/python3.6
 from django.core import management
 
 if __name__ == "__main__":
