08270a994e83160df9cea7d54b63502abbc2fba8
Steven Wohl <swohl@template-debian-8.6.0-amd64>
2018-09-14 13:53:43 +0200

    change file size

--- micromic/api/validators.py
+++ micromic/api/validators.py
@@ -4,7 +4,7 @@ from django.core.exceptions import ValidationError
 def validate_file_size(value):
     filesize= value.size
 
-    if filesize > 1048576:
+    if filesize > 2048576:
         raise ValidationError("The maximum file size that can be uploaded is 1MB")
     else:
         return value
